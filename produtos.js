a8529bc82d5c42aeed86c3412ce1bc43a99f945a
Bruno Lima <bruno.lima721@gmail.com>
2020-10-14 20:50:46 -0300

    alterando itens

--- produtos.js
+++ produtos.js
@@ -1,9 +1,13 @@
-function exibir_produtos (){
-    
-alert("ok")
-}
-function produtos (){
-    document.getElementsByClassName(img)
-    
+function exibir_eletro(produtos) {
+
+    let itens = document.getElementsByClassName('fotos_produtos');
+    console.log(itens);
+    for (let i = 0; i < itens.length; i++) {
+        console.log(itens[i].id)
+        if (produtos == itens[i].id)
+        itens[i].style = "display:block";
+        else
+        itens[i].style = "display:none";
+    }
 
 }
